0dc4026eac9fd2aeafb9ee515954cc927a4f2830
peishuangheng <peishuangeng@hualala.com>
2023-08-03 18:27:38 +0800

    feat: init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const { name } = require('./package.json');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    port: 8082,
+    headers:{
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      chunkLoadingGlobal: `webpackJsonp_${name}`,
+    },
+  },
 })
